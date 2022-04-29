4cb6028ecb840733bb452409ff94b5695076c6d1
sonergezgen <soner.gezgen@student.ehb.be>
2022-04-29 02:56:36 +0200

    removing unnec. comments

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
 <div class="background-image grid grid-cols-1 m-auto">
     <div class="flex text-gray-100 pt-10">
         <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
-            <h1 class="sm:text-whote text-4xl uppercase font-bold text-shadow-md pb-14">Share your story with the world...!!!!</h1>
+            <h1 class="sm:text-whote text-4xl uppercase font-bold text-shadow-md pb-14">Share your story with the world.</h1>
             <!--<a href="/blog" class="text-center bg-gray-50 text-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase"> Read More </a>-->
         </div>
     </div>
